93d8cc8e21bdfdc8294f3f7051019aab6358e88e
Navagod Chanprempee <navagod@orisma.com>
2016-09-29 18:25:43 +0700

    demo2 autoresize

--- demo/demo2.js
+++ demo/demo2.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('#myTable05').fixedHeaderTable({ altClass: 'odd', footer: false, fixedColumns: 1 });
+    $('#myTable05').fixedHeaderTable({ altClass: 'odd', footer: false, fixedColumns: 1});
  //    $('.fht-fixed-body .fht-tbody').mCustomScrollbar({
 	//     axis:"yx"
 	// });
